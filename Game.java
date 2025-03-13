6a725e9104b9b57e15e02ec251fb978f92224b82
arnieS06 <sharma.arnav2016@gmail.com>
2025-03-13 15:37:44 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 public class Game {
+
     private String name = "none";
     Rule[] rules = new Rule[100];
     private int numRules = 0;
@@ -21,12 +22,51 @@ public class Game {
             }
         }
 
-        if (rsLength == rulesRemainingSpaces) {
+        if (rsLength <= rulesRemainingSpaces) {
             int emptyIndex = (rules.length - rulesRemainingSpaces) - 1; // subtracting 1 because arrays are [length - 1]
+            int rsIndex = 0;
 
             for (int i = emptyIndex; i < rulesRemainingSpaces; i++) {
-
+                rules[emptyIndex] = rs[rsIndex];
+                rsIndex++;
             }
+
+            numRules += rsLength;
+        }
+
+        else {
+            System.out.println("ERROR: not enough remaining space for given array");
+        }
+    }
+
+    public Game(String n, Rule[] rs, int t, int ts) {
+        this.name = n;
+        this.teams = t;
+        this.teamSize = ts;
+
+        int rsLength = rs.length;
+        int rulesRemainingSpaces = 0;
+
+        for (int i = 0; i < rules.length; i++) {
+            if (rules[i] == null) {
+                rulesRemainingSpaces++;
+            }
+        }
+
+        if (rsLength <= rulesRemainingSpaces) {
+            int emptyIndex = (rules.length - rulesRemainingSpaces) - 1; // subtracting 1 because arrays are [length - 1]
+            int rsIndex = 0;
+
+            for (int i = emptyIndex; i < rulesRemainingSpaces; i++) {
+                rules[emptyIndex] = rs[rsIndex];
+                rsIndex++;
+            }
+
+            numRules += rsLength;
+        }
+
+        else {
+            System.out.println("ERROR: not enough remaining space for given array");
         }
     }
 
@@ -54,6 +94,38 @@ public class Game {
     public void setTeamSize(int set) {
         this.teams = set;
     }
+
+    public boolean addRule(Rule r) {
+        int rulesRemainingSpaces = 0;
+
+        for (int i = 0; i < rules.length; i++) {
+            if (rules[i] == null) {
+                rulesRemainingSpaces++;
+            }
+        }
+
+        if (rulesRemainingSpaces >= 1) {
+            int emptyIndex = (rules.length - rulesRemainingSpaces) - 1; // subtracting 1 because arrays are [length - 1]
+            rules[emptyIndex] = r;
+            numRules++;
+
+            return true;
+        }
+
+        else {
+            //System.out.println("Error: not enough space");
+            return false;
+        }
+    }
+
+    public Rule[] getRules() {
+        return rules;
+    }
+
+    public int getNumRules() {
+        return numRules;
+    }
+
 
 
 }
